c597846106a39b2976a69dfba213a29b3c41f52a
Isaac Shapira <fresheyeball@protonmail.com>
2021-08-16 17:59:33 -0600

    refactor types

--- src/Control/Concurrent/TokenLimiter/Concurrent.hs
+++ src/Control/Concurrent/TokenLimiter/Concurrent.hs
@@ -1,11 +1,14 @@
 {-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE NumericUnderscores #-}
 {-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE DataKinds #-}
 
 module Control.Concurrent.TokenLimiter.Concurrent
-  ( Count,
+  ( Count, getCount, CountType(..),
     TokenLimitConfig (..),
-    MonotonicTime,
+    MonotonicTime, MonotonicTimeType(..),
     TokenLimiter (..),
     makeTokenLimiter,
     canDebit,
@@ -19,28 +22,37 @@ where
 
 import Control.Concurrent
 import Data.Word
-import GHC.Clock
+import qualified GHC.Clock as Clock
 import GHC.Generics (Generic)
 import Numeric.Natural
 
+
+-- | Make the Count type semantic with it's usage
+data CountType = Initial | Max | PerSecond | LastServiced | Current | Debit
+
 -- | An amount of tokens
-type Count = Word64
+newtype Count (ct :: CountType) = Count { getCount :: Word64 }
+  deriving (Show, Eq, Ord, Enum, Num, Real, Integral, Generic)
 
 -- | A configuration for 'TokenLimiter'
 data TokenLimitConfig = TokenLimitConfig
   { -- | How many tokens should be in the bucket when it's created
-    tokenLimitConfigInitialTokens :: !Count,
+    tokenLimitConfigInitialTokens :: !(Count 'Initial),
     -- | Maximum number of tokens the bucket can hold at any one time
-    tokenLimitConfigMaxTokens :: !Count,
+    tokenLimitConfigMaxTokens :: !(Count 'Max),
     -- | How many tokens are added to the bucket per second
-    tokenLimitConfigTokensPerSecond :: !Count
+    tokenLimitConfigTokensPerSecond :: !(Count 'PerSecond)
   }
   deriving (Show, Eq, Generic)
+
+-- | Make MonotonicTime semantic with it's usage
+data MonotonicTimeType = LastServicedTime | Now
 
 -- | A type synonym for a number of "monotonic time" nanoseconds.
 --
 -- This only exists because it is also a 'Word64' and would be too easy to confuse with a 'Count'.
-type MonotonicTime = Word64
+newtype MonotonicTime (m :: MonotonicTimeType) = MonotonicTime { getMonotonicTime :: Word64 }
+  deriving (Eq, Ord, Show, Enum, Num, Real, Integral, Generic)
 
 -- | A token bucket-based rate limiter
 --
@@ -54,61 +66,76 @@ data TokenLimiter = TokenLimiter
     --
     -- Not that this library assumes that you never put anything into this mvar
     -- yourself and only use the functions in this library to interact with it.
-    tokenLimiterLastServiced :: !(MVar (MonotonicTime, Count))
+    tokenLimiterLastServiced :: !(MVar (MonotonicTime 'LastServicedTime, Count 'LastServiced))
   }
   deriving (Eq, Generic)
+
+
+getLastServiced :: Count 'Initial -> Count 'Max -> Count 'LastServiced
+getLastServiced (Count initial) (Count max') = Count $ initial `min` max'
+
+
+lastServicedIsNow :: MonotonicTime 'Now -> MonotonicTime 'LastServicedTime
+lastServicedIsNow (MonotonicTime now) = MonotonicTime now
+
+
+getMonotonicTimeNSec :: IO (MonotonicTime 'Now)
+getMonotonicTimeNSec = MonotonicTime <$> Clock.getMonotonicTimeNSec
+
 
 -- | Make a token limiter
 --
 -- The initial number of tokens will be the minimum of the 'tokenLimitConfigInitialTokens' and the 'tokenLimitConfigMaxTokens',
 makeTokenLimiter :: TokenLimitConfig -> IO TokenLimiter
 makeTokenLimiter tokenLimiterConfig = do
-  now <- getMonotonicTimeNSec
-  tokenLimiterLastServiced <- newMVar (now, min (tokenLimitConfigInitialTokens tokenLimiterConfig) (tokenLimitConfigMaxTokens tokenLimiterConfig))
+  MonotonicTime now <- getMonotonicTimeNSec
+  tokenLimiterLastServiced <- newMVar (MonotonicTime now, getLastServiced (tokenLimitConfigInitialTokens tokenLimiterConfig) (tokenLimitConfigMaxTokens tokenLimiterConfig))
   pure TokenLimiter {..}
+
+
+enoughAvailable :: Count 'Current -> Count 'Debit -> Bool
+enoughAvailable (Count currentCount) (Count debit) = currentCount >= debit
+
+
+getNewCount :: Count 'Current -> Count 'Debit -> Count 'LastServiced
+getNewCount (Count currentCount) (Count debit) = Count $ currentCount - debit
+
 
 -- | Ask if we could debit a number of tokens, without actually doing it.
 --
 -- Note that this information can become stale _very_ quickly.
 -- If you want to also actually debit a number of tokens, use 'tryDebit' instead.
-canDebit :: TokenLimiter -> Word64 -> IO Bool
+canDebit :: TokenLimiter -> Count 'Debit -> IO Bool
 canDebit TokenLimiter {..} debit = withMVar tokenLimiterLastServiced $ \(lastServiced, countThen) -> do
   now <- getMonotonicTimeNSec
   let currentCount = computeCurrentCount tokenLimiterConfig lastServiced countThen now
-  let enoughAvailable = currentCount >= debit
-  pure enoughAvailable
+  pure $ enoughAvailable currentCount debit
 
 -- | Check if we can debit a number of tokens, and do it if possible.
 --
 -- The returned boolean represents whether the tokens were debited.
-tryDebit :: TokenLimiter -> Word64 -> IO Bool
+tryDebit :: TokenLimiter -> Count 'Debit -> IO Bool
 tryDebit TokenLimiter {..} debit = modifyMVar tokenLimiterLastServiced $ \(lastServiced, countThen) -> do
   now <- getMonotonicTimeNSec
   let currentCount = computeCurrentCount tokenLimiterConfig lastServiced countThen now
-  let enoughAvailable = currentCount >= debit
-  if enoughAvailable
-    then do
-      let newCount = currentCount - debit
-      pure ((now, newCount), True)
-    else pure ((lastServiced, countThen), False)
+  pure $ if enoughAvailable currentCount debit
+    then ((lastServicedIsNow now, getNewCount currentCount debit), True)
+    else ((lastServiced, countThen), False)
 
 -- | Wait until the given number of tokens can be debited
-waitDebit :: TokenLimiter -> Word64 -> IO ()
+waitDebit :: TokenLimiter -> Count 'Debit -> IO ()
 waitDebit TokenLimiter {..} debit = modifyMVar_ tokenLimiterLastServiced $ \(lastServiced, countThen) -> do
   now <- getMonotonicTimeNSec
   let currentCount = computeCurrentCount tokenLimiterConfig lastServiced countThen now
-  let enoughAvailable = currentCount >= debit
-  if enoughAvailable
-    then do
-      let newCount = currentCount - debit
-      pure (now, newCount)
+      extraTokensNeeded (Count debit) (Count currentCount) = Count $ debit - currentCount
+  if enoughAvailable currentCount debit
+    then pure (lastServicedIsNow now, getNewCount currentCount debit)
     else do
-      let extraTokensNeeded = debit - currentCount
       let microsecondsToWaitDouble :: Double
           microsecondsToWaitDouble =
             1_000_000
               -- fromIntegral :: Word64 -> Double
-              * fromIntegral extraTokensNeeded
+              * fromIntegral (extraTokensNeeded debit currentCount)
               -- fromIntegral :: Word64 -> Double
               / fromIntegral (tokenLimitConfigTokensPerSecond tokenLimiterConfig)
       let microsecondsToWait = ceiling microsecondsToWaitDouble
@@ -123,29 +150,30 @@ waitDebit TokenLimiter {..} debit = modifyMVar_ tokenLimiterLastServiced $ \(las
       -- atomicity if there are no other producers for this MVar, which there
       -- aren't.
       let currentCountAfterWaiting = computeCurrentCount tokenLimiterConfig lastServiced countThen nowAfterWaiting
-      let newCount = currentCountAfterWaiting - debit
-      pure (nowAfterWaiting, newCount)
+      pure (lastServicedIsNow nowAfterWaiting, getNewCount currentCountAfterWaiting debit)
 
 -- | Compute the current number of tokens in a bucket purely.
 --
 -- You should not need this function.
-computeCurrentCount :: TokenLimitConfig -> MonotonicTime -> Count -> MonotonicTime -> Count
+computeCurrentCount :: TokenLimitConfig -> MonotonicTime 'LastServicedTime -> Count 'LastServiced -> MonotonicTime 'Now -> Count 'Current
 computeCurrentCount TokenLimitConfig {..} lastServiced countThen now =
-  let nanoDiff :: Word64
-      nanoDiff = now - lastServiced
-      countToAddDouble :: Double
-      countToAddDouble =
+
+  let countToAdd, totalCount :: Word64
+      countToAdd = floor $
+
         -- fromIntegral :: Word64 -> Double
-        fromIntegral nanoDiff
+        fromIntegral (getMonotonicTime now - getMonotonicTime lastServiced)
           -- fromIntegral :: Word64 -> Double
           * fromIntegral tokenLimitConfigTokensPerSecond
           / 1_000_000_000
-      countToAdd :: Word64
-      countToAdd = floor countToAddDouble
+
+      totalCount = getCount countThen + countToAdd
+
       totalPrecise :: Natural
       totalPrecise = fromIntegral countThen + fromIntegral countToAdd
+
       willOverflow = totalPrecise > fromIntegral (maxBound :: Word64)
-      totalCount = countThen + countToAdd
-   in if willOverflow
-        then tokenLimitConfigMaxTokens
-        else min tokenLimitConfigMaxTokens totalCount
+
+   in Count $ if willOverflow
+        then getCount tokenLimitConfigMaxTokens
+        else getCount tokenLimitConfigMaxTokens `min` totalCount
