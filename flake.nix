fcc95f137696ddf23b63f32a6d697ba5a83a51b2
Tom Sydney Kerckhove <syd@cs-syd.eu>
2025-05-31 17:41:25 +0200

    Simplify flake

--- flake.nix
+++ flake.nix
@@ -4,21 +4,20 @@
     nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-25.05";
     nixpkgs-24_11.url = "github:NixOS/nixpkgs?ref=nixos-24.11";
     nixpkgs-24_05.url = "github:NixOS/nixpkgs?ref=nixos-24.05";
-    nixpkgs-23_11.url = "github:NixOS/nixpkgs?ref=nixos-23.11";
     horizon-advance.url = "git+https://gitlab.horizon-haskell.net/package-sets/horizon-advance";
     pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
-    fast-myers-diff.url = "github:NorfairKing/fast-myers-diff";
-    fast-myers-diff.flake = false;
-    validity.url = "github:NorfairKing/validity";
-    validity.flake = false;
-    autodocodec.url = "github:NorfairKing/autodocodec";
     autodocodec.flake = false;
-    safe-coloured-text.url = "github:NorfairKing/safe-coloured-text";
-    safe-coloured-text.flake = false;
-    sydtest.url = "github:NorfairKing/sydtest";
-    sydtest.flake = false;
-    opt-env-conf.url = "github:NorfairKing/opt-env-conf";
+    autodocodec.url = "github:NorfairKing/autodocodec";
+    fast-myers-diff.flake = false;
+    fast-myers-diff.url = "github:NorfairKing/fast-myers-diff";
     opt-env-conf.flake = false;
+    opt-env-conf.url = "github:NorfairKing/opt-env-conf";
+    safe-coloured-text.flake = false;
+    safe-coloured-text.url = "github:NorfairKing/safe-coloured-text";
+    sydtest.flake = false;
+    sydtest.url = "github:NorfairKing/sydtest";
+    validity.flake = false;
+    validity.url = "github:NorfairKing/validity";
   };
 
   outputs =
@@ -26,27 +25,26 @@
     , nixpkgs
     , nixpkgs-24_11
     , nixpkgs-24_05
-    , nixpkgs-23_11
     , horizon-advance
     , pre-commit-hooks
-    , fast-myers-diff
-    , validity
-    , safe-coloured-text
     , autodocodec
-    , sydtest
+    , fast-myers-diff
     , opt-env-conf
+    , safe-coloured-text
+    , sydtest
+    , validity
     }:
     let
       system = "x86_64-linux";
       nixpkgsFor = nixpkgs: import nixpkgs { inherit system; config.allowUnfree = true; config.allowBroken = true; };
       pkgs = nixpkgsFor nixpkgs;
       allOverrides = pkgs.lib.composeManyExtensions [
+        (pkgs.callPackage (autodocodec + "/nix/overrides.nix") { })
         (pkgs.callPackage (fast-myers-diff + "/nix/overrides.nix") { })
+        (pkgs.callPackage (opt-env-conf + "/nix/overrides.nix") { })
         (pkgs.callPackage (safe-coloured-text + "/nix/overrides.nix") { })
         (pkgs.callPackage (sydtest + "/nix/overrides.nix") { })
-        (pkgs.callPackage (opt-env-conf + "/nix/overrides.nix") { })
         (pkgs.callPackage (validity + "/nix/overrides.nix") { })
-        (pkgs.callPackage (autodocodec + "/nix/overrides.nix") { })
         self.overrides.${system}
       ];
       horizonPkgs = horizon-advance.legacyPackages.${system}.extend allOverrides;
@@ -59,18 +57,17 @@
       packages.${system}.default = pkgs.haskellPackages.token-limiter-concurrent;
       checks.${system} =
         let
-          backwardCompatibilityCheckFor = nixpkgs: (haskellPackagesFor nixpkgs).autodocodecRelease;
+          backwardCompatibilityCheckFor = nixpkgs: (haskellPackagesFor nixpkgs).token-limiter-concurrent;
           allNixpkgs = {
             inherit
               nixpkgs-24_11
-              nixpkgs-24_05
-              nixpkgs-23_11;
+              nixpkgs-24_05;
           };
           backwardCompatibilityChecks = pkgs.lib.mapAttrs (_: nixpkgs: backwardCompatibilityCheckFor nixpkgs) allNixpkgs;
         in
         backwardCompatibilityChecks // {
           release = self.packages.${system}.default;
-          forwardCompatibility = horizonPkgs.autodocodecRelease;
+          forwardCompatibility = horizonPkgs.token-limiter-concurrent;
           pre-commit = pre-commit-hooks.lib.${system}.run {
             src = ./.;
             hooks = {
